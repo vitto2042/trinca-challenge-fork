ced17eedad83ca8b27f5ecf9a40f01eb9b14ff64
Vitto Franciscatto <vfranciscatto@1500fh.com>
2023-02-14 10:52:29 -0300

    When bbq gets declined, all pending invites from it are declined too

--- Serverless-Api/Functions/Bbq/ModerateBbqs/RunModerateBbq.cs
+++ Serverless-Api/Functions/Bbq/ModerateBbqs/RunModerateBbq.cs
@@ -20,26 +20,59 @@ namespace Serverless_Api
             _repository = repository;
         }
 
+        private List<string> GetPeopleMinusModerators(Lookups lookups)
+        {
+            List<string> peopleMinusMods = new List<string>();
+            peopleMinusMods.AddRange(lookups.PeopleIds);
+            peopleMinusMods.RemoveAll(x => lookups.ModeratorIds.Contains(x));
+
+            return peopleMinusMods;
+        }
+
+        private async Task SendInvites(Bbq bbq, List<string> invitedPeople)
+        {
+            foreach (var personId in invitedPeople)
+            {
+                Person person = await _persons.GetAsync(personId);
+                PersonHasBeenInvitedToBbq @event = new PersonHasBeenInvitedToBbq(bbq.Id, bbq.Date, bbq.Reason);
+                person.Apply(@event);
+                await _persons.SaveAsync(person);
+            }
+        }
+
+        private async Task RejectAllPendingInvites(string bbqId, List<string> people)
+        {
+            foreach (var personId in people)
+            {
+                Person person = await _persons.GetAsync(personId);
+                List<InviteWasDeclined> @events = person.Invites
+                    .Where(x => x.Id == bbqId
+                            && x.Status == InviteStatus.Pending)
+                    .Select(x => new InviteWasDeclined { InviteId = x.Id, PersonId = person.Id })
+                    .ToList();
+                @events.ForEach(x => person.Apply(x));
+                await _persons.SaveAsync(person);
+            }
+        }
+
         [Function(nameof(RunModerateBbq))]
         public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "put", Route = "churras/{id}/moderar")] HttpRequestData req, string id)
         {
-            var moderationRequest = await req.Body<ModerateBbqRequest>();
+            ModerateBbqRequest moderationRequest = await req.Body<ModerateBbqRequest>();
 
-            var bbq = await _repository.GetAsync(id);
+            Bbq bbq = await _repository.GetAsync(id);
 
             bbq.Apply(new BbqStatusUpdated(moderationRequest.GonnaHappen, moderationRequest.TrincaWillPay));
 
             var lookups = await _snapshots.AsQueryable<Lookups>("Lookups").SingleOrDefaultAsync();
 
-            var peopleMinusMods = lookups.PeopleIds;
-            peopleMinusMods.RemoveAll(x => lookups.ModeratorIds.Contains(x));
-
-            foreach (var personId in peopleMinusMods)
+            if (moderationRequest.GonnaHappen)
             {
-                var person = await _persons.GetAsync(personId);
-                var @event = new PersonHasBeenInvitedToBbq(bbq.Id, bbq.Date, bbq.Reason);
-                person.Apply(@event);
-                await _persons.SaveAsync(person);
+                await SendInvites(bbq, GetPeopleMinusModerators(lookups));
+            }
+            else
+            {
+                await RejectAllPendingInvites(bbq.Id, lookups.ModeratorIds);
             }
 
             await _repository.SaveAsync(bbq);
